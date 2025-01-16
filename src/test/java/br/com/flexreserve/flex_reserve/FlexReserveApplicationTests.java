a8c555b0c4b7fd153d510e2d71ab953eb03de09e
yurih1 <yurih2009@gmail.com>
2025-01-15 21:45:47 -0300

    feat: start project

---
+++ src/test/java/br/com/flexreserve/flex_reserve/FlexReserveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.flexreserve.flex_reserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexReserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
